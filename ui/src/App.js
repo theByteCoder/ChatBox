c5e8a7ccdfc07c35e8c50b71969299421c6e3724
theByteCoder <47722399+theByteCoder@users.noreply.github.com>
2021-06-06 20:37:23 +0530

    feat(app): Added close notfication feature

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppWithRouterAccess from "./Components/AppWithRouterAccess";
 import { SnackbarProvider } from "notistack";
+import { Button } from "@material-ui/core";
 
 const App = () => {
+  const notistackRef = useRef();
   return (
     <SnackbarProvider
+      ref={notistackRef}
+      action={(key) => (
+        <Button
+          onClick={() => notistackRef.current.closeSnackbar(key)}
+          style={{ color: "#fff", fontSize: "20px" }}
+        >
+          ✖
+        </Button>
+      )}
       maxSnack={10}
       anchorOrigin={{
         vertical: "top",
